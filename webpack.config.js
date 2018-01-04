c13501042f124d90a0a88ed2b2cf440435787826
KLAPDOOS\Thijs <thijsdevos@outlook.com>
2018-01-04 23:05:06 +0100

    Removed npm warnings during building

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,9 @@ module.exports = {
                 exclude: [/node_modules/],
                 use: [{
                     loader: 'babel-loader',
-                    options: { presets: ['es2015', 'stage-2'] },
+                    options: {
+                        presets: ['babel-preset-env']
+                    },
                 }],
             },
             {
